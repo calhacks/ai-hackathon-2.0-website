16a7a30a0abaa5161008aa9bffc68b7f6a227376
aryanvichare <aryanvichare10@gmail.com>
2024-03-24 01:44:01 -0700

    Please look good 🫠

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
       <div className="absolute right-[120px] top-[80px] hidden text-6xl text-white md:block">
         <Logos />
       </div>
-      <div className="relative z-10 mx-auto flex min-h-[100vh] max-w-screen-xl flex-col px-8 pb-12 pt-36 sm:px-16 md:place-content-between md:justify-between md:px-24 md:py-36">
+      <div className="md:items-between relative z-10 mx-auto flex min-h-[100vh] max-w-screen-xl flex-col items-center px-8 pb-12 pt-36 sm:px-16 md:place-content-between md:items-start md:justify-between md:px-24 md:py-36">
         <div className="relative flex flex-col items-center text-center md:items-start md:text-left">
           <div className="max-w-md">
             <h1 className="font-pp-neue-bit flex flex-col text-5xl font-bold text-white sm:text-7xl md:text-8xl">
@@ -36,13 +36,13 @@ const Hero = () => {
               </motion.span>
             </h1>
           </div>
-          <p className="font-whyte-inktrap inline-block text-xl font-semibold tracking-wider text-electric-blue [text-shadow:_0px_0px_3px_rgb(152_249_255)] sm:text-2xl md:text-3xl">
+          <p className="font-whyte-inktrap inline-block w-full text-xl font-semibold tracking-wider text-electric-blue [text-shadow:_0px_0px_3px_rgb(152_249_255)] sm:text-2xl md:text-3xl">
             June 22-23, 2024
           </p>
           <Logos className="mx-auto mt-8 block flex flex-row md:hidden" />
         </div>
-        <form className="font-whyte-inktrap ml-auto mt-32 w-full max-w-[19rem] text-white sm:max-w-sm md:mt-0 md:w-1/2">
-          <p className="text-center text-lg text-white sm:whitespace-nowrap md:text-right md:text-xl">
+        <form className="font-whyte-inktrap mt-32 w-full w-full max-w-md text-white md:ml-auto md:mt-0">
+          <p className="text-center text-center text-lg text-white sm:whitespace-nowrap md:text-right md:text-right md:text-xl">
             Be the first to know when applications launch.
           </p>
           <div className="mt-4 flex flex-row space-x-4">
