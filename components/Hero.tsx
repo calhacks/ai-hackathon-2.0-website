e343d5b4949ee6682bc57059f0ff53cf6f666fb1
aryanvichare <aryanvichare10@gmail.com>
2024-03-24 01:09:28 -0700

    Fix more responsiveness issues

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,10 +12,10 @@ const Hero = () => {
       <div className="absolute right-[120px] top-[80px] hidden text-6xl text-white md:block">
         <Logos />
       </div>
-      <div className="relative z-10 mx-auto flex min-h-[100vh] max-w-screen-xl flex-col px-24 pb-12 pt-36 md:place-content-between md:justify-between md:py-36">
+      <div className="relative z-10 mx-auto flex min-h-[100vh] max-w-screen-xl flex-col px-16 pb-12 pt-36 md:place-content-between md:justify-between md:px-24 md:py-36">
         <div className="relative flex flex-col items-center text-center md:items-start md:text-left">
           <div className="max-w-md">
-            <h1 className="font-pp-neue-bit inline-flex flex-col text-8xl font-bold leading-[3.5rem] text-white">
+            <h1 className="font-pp-neue-bit inline-flex flex-col text-7xl font-bold leading-[3.5rem] text-white md:text-8xl">
               <span>UC Berkeley</span>
               <span title="AI Hackathon" className="glitch-text">
                 AI Hackathon
@@ -36,13 +36,13 @@ const Hero = () => {
               </motion.span>
             </h1>
           </div>
-          <p className="font-whyte-inktrap	mt-6 inline-block text-3xl font-semibold tracking-wider text-electric-blue [text-shadow:_0px_0px_3px_rgb(152_249_255)]">
+          <p className="font-whyte-inktrap mt-4 inline-block text-2xl font-semibold tracking-wider text-electric-blue [text-shadow:_0px_0px_3px_rgb(152_249_255)] md:mt-6 md:text-3xl">
             June 22-23, 2024
           </p>
           <Logos className="mx-auto mt-8 block flex flex-row md:hidden" />
         </div>
         <form className="font-whyte-inktrap ml-auto mt-32 w-full text-7xl text-white md:mt-0 md:w-1/2">
-          <p className="whitespace-nowrap text-center text-xl text-white md:text-right">
+          <p className="whitespace-nowrap text-center text-lg text-white md:text-right md:text-xl">
             Be the first to know when applications launch.
           </p>
           <div className="mt-4 flex flex-row space-x-4">
@@ -58,13 +58,14 @@ const Hero = () => {
               Notify Me
             </button>
           </div>
-          <div className="mt-2 flex justify-center text-lg font-normal text-gray-200 md:justify-end">
+          <div className="mt-2 flex justify-center text-base font-normal text-gray-200 md:justify-end md:text-lg">
             <span className="text-gray-100">Questions? Email</span>
             <a
               href="mailto:team@calhacks.io"
               className={`
-              ml-1 bg-gradient-to-r from-electric-blue to-heliotrope bg-clip-text text-transparent after:absolute after:block
-              after:h-[3px] after:w-[145px] after:origin-left after:scale-x-0 after:bg-heliotrope after:transition after:duration-300 after:content-[''] after:hover:scale-x-100
+              relative ml-1
+              inline-block bg-gradient-to-r from-electric-blue to-heliotrope bg-clip-text text-transparent
+              after:absolute after:block after:h-[3px] after:w-full after:origin-left after:scale-x-0 after:bg-heliotrope after:transition after:duration-300 after:content-[''] after:hover:scale-x-100
               `}
             >
               team@calhacks.io
