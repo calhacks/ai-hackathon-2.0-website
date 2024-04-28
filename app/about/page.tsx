60c907eb278741e7ad765b83aa73045a46c56d84
Sam Poder <39828164+sampoder@users.noreply.github.com>
2024-04-28 13:16:13 -0700

    Nav and click!

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import SlidingText from "@/components/SlidingText";
+import Nav from "@/components/Nav";
 
 const cardBackground = { backgroundImage: 'linear-gradient(90deg, rgba(76, 4, 110, 0.2508) 0.25%, rgba(27, 6, 152, 0.1914) 93.27%)' }
 
 export default function Home() {
   return (
     <>
+      <Nav />
       <main className="z-1 relative min-h-screen overflow-hidden flex flex-col items-center text-white" style={{ zIndex: 999 }}>
         <img src="/images/four-point.png" className="mt-24" style={{ height: '50px' }} />
         <h1 className="text-5xl mt-6 font-pp-mondwest">
