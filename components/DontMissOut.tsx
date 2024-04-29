882dc2fe7f9fcefb062c5c3720a368b8746d44d9
Prabhas Kurapati <prabhask@berkeley.edu>
2024-04-29 01:00:56 -0700

    fix styling of sponsors and footer

--- components/DontMissOut.tsx
+++ components/DontMissOut.tsx
@@ -4,21 +4,17 @@ import Image from "next/image";
 
 export default function DontMissOut() {
   return (
-    <div className="bg-black py-28">
-      <p className="mb-6 text-center font-pp-mondwest text-4xl text-white">
+    <div className="bg-black px-10 py-28 md:px-0">
+      <p className="mb-6 text-center font-pp-mondwest text-3xl text-white sm:text-4xl xl:text-5xl">
         Don&apos;t Miss Out.
       </p>
-      <Image
-        src="/images/date.svg"
-        alt="9am June 22 (Saturday) - 6pm June 23 (Sunday)"
-        width={475}
-        height={200}
-        className="mx-auto mb-1"
-      />
-      <p className="mb-16 text-center text-xl text-white">
+      <p className="mb-1 text-center text-base text-white sm:text-xl xl:text-2xl">
+        9am June 22 (Saturday) - 6pm June 23 (Sunday)
+      </p>
+      <p className="mb-16 text-center text-base text-white sm:text-xl xl:text-2xl">
         UC Berkeley, MLK Student Union Building
       </p>
-      <button className="mx-auto block h-[48px] w-[138px] bg-white text-xl text-opacity-0 hover:bg-[#e6e6e6]">
+      <button className="mx-auto block h-[38.4px] w-[110.4px] bg-white text-base text-opacity-0 hover:bg-[#e6e6e6] sm:h-[48px] sm:w-[138px] sm:text-xl xl:h-[60px] xl:w-[172.5px] xl:text-2xl">
         Register
       </button>
     </div>
--- components/SponsorsPartners.tsx
+++ components/SponsorsPartners.tsx
@@ -3,28 +3,76 @@
 import Image from "next/image";
 
 const sponsorList = [
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
-  { image: "/images/adobe.svg", link: "https://www.adobe.com/" },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
+  {
+    image: "/images/adobe.svg",
+    link: "https://www.adobe.com/",
+    alt: "Adobe sponsor",
+  },
 ];
 
 export default function SponsorsPartners() {
   return (
-    <div className="flex h-screen w-screen bg-black">
+    <div className="flex w-screen bg-black px-4 pb-16 pt-4 md:px-0 lg:h-screen lg:pb-0">
       <div className="m-auto">
-        <p className="mb-2 text-center font-pp-mondwest text-4xl text-white">
+        <p className="mb-2 text-center font-pp-mondwest text-3xl text-white sm:text-4xl xl:text-5xl">
           Sponsors & Partners
         </p>
-        <p className="text-center text-xl text-white">
+        <p className="text-center text-base text-white sm:text-xl xl:text-2xl">
           A round of applause for our sponsors, who make all of this possible.
         </p>
         <div className="my-16 flex flex-row justify-center gap-4">
@@ -32,28 +80,28 @@ export default function SponsorsPartners() {
             href="https://hackberkeley.org"
             rel="noopener noreferrer"
             target="_blank"
-            className="relative z-50 m-0 -mt-1.5 h-[100px] w-[200px] p-0"
+            className="relative z-50 m-0 -mt-1.5 h-[80px] w-[160px] p-0 sm:h-[100px] sm:w-[200px] xl:h-[125px] xl:w-[250px]"
           >
             <Image
               src="/images/hab-logo.svg"
               alt="Hackathons @ Berkeley Logo"
-              fill={true}
+              fill
             />
           </a>
           <a
             href="https://skydeck.berkeley.edu/"
             rel="noopener noreferrer"
             target="_blank"
-            className="relative z-50 m-0 h-[70px] w-[200px] p-0"
+            className="relative z-50 m-0 h-[56px] w-[160px] p-0 sm:h-[70px] sm:w-[200px] xl:h-[87.5px] xl:w-[250px]"
           >
             <Image
               src="/images/berkeley-skydeck-logo.png"
               alt="Berkeley Skydeck Logo"
-              fill={true}
+              fill
             />
           </a>
         </div>
-        <div className="grid grid-cols-2 gap-10 2xl:grid-cols-4">
+        <div className="grid grid-cols-2 justify-items-center gap-10 lg:grid-cols-4">
           {sponsorList.map((logo, index) => {
             return (
               <a
@@ -61,14 +109,9 @@ export default function SponsorsPartners() {
                 href={logo.link}
                 rel="noopener noreferrer"
                 target="_blank m-auto"
-                className="border border-white p-2"
+                className="relative h-20 w-40 md:h-24 md:w-48 lg:h-20 lg:w-40 xl:h-28 xl:w-56"
               >
-                <Image
-                  width={220}
-                  height={100}
-                  src={logo.image}
-                  alt="Hackathons @ Berkeley Logo"
-                />
+                <Image fill src={logo.image} alt={logo.alt} />
               </a>
             );
           })}
