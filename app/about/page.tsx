c3581354b36ff84509df8b23c49ee76568053ff4
sauhardjain <sauhardjain03@gmail.com>
2024-04-30 15:20:16 -0700

    styling: gradients on about and contact pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,56 +2,89 @@ import Hero from "@/components/Hero";
 import SlidingText from "@/components/SlidingText";
 import Nav from "@/components/Nav";
 
-const cardBackground = { backgroundImage: 'linear-gradient(90deg, rgba(76, 4, 110, 0.2508) 0.25%, rgba(27, 6, 152, 0.1914) 93.27%)' }
+const cardBackground = {
+  backgroundImage:
+    "linear-gradient(90deg, rgba(76, 4, 110, 0.2508) 0.25%, rgba(27, 6, 152, 0.1914) 93.27%)",
+};
 
 export default function Home() {
   return (
-    <>
+    <main className="bg-[url('../public/images/about-bg.svg')] bg-cover">
       <Nav />
-      <main className="z-1 relative min-h-screen overflow-hidden flex flex-col items-center text-white" style={{ zIndex: 999 }}>
-        <img src="/images/four-point.png" className="mt-24" style={{ height: '50px' }} />
-        <h1 className="text-5xl mt-6 font-pp-mondwest">
-          About
-        </h1>
-        <p className="max-w-2xl text-xl text-center text-balance mt-4">
-          Cal Hacks, Berkeley SkyDeck, and UC Berkeley are joining forces again to give you an unparalleled hacking experience.  
+      <main
+        className="z-1 relative flex min-h-screen flex-col items-center overflow-hidden text-white"
+        style={{ zIndex: 999 }}
+      >
+        <img
+          src="/images/four-point.png"
+          className="mt-24"
+          style={{ height: "50px" }}
+        />
+        <h1 className="mt-6 font-pp-mondwest text-5xl">About</h1>
+        <p className="mt-4 max-w-2xl text-balance text-center text-xl">
+          Cal Hacks, Berkeley SkyDeck, and UC Berkeley are joining forces again
+          to give you an unparalleled hacking experience.
         </p>
-        <div className="grid md:grid-cols-3 w-95 md:w-full max-w-5xl gap-6 mt-6 px-8">
-          <div style={cardBackground} className="p-6 rounded-lg">
-            <img src="/images/berkeley.png" style={{ height: '150px' }} className="mx-auto" />
+        <div className="w-95 mt-6 grid max-w-5xl gap-6 px-8 md:w-full md:grid-cols-3">
+          <div style={cardBackground} className="rounded-lg p-6">
+            <img
+              src="/images/berkeley.png"
+              style={{ height: "150px" }}
+              className="mx-auto"
+            />
             <h4 className="mt-4 text-lg font-bold text-electric-blue">
               UC Berkeley
             </h4>
             <p className="mt-2">
-              is the world’s #1 public university, with a rich history of groundbreaking discoveries, knowledge, and innovation.
+              is the world’s #1 public university, with a rich history of
+              groundbreaking discoveries, knowledge, and innovation.
             </p>
           </div>
-          <div style={cardBackground} className="p-6 rounded-lg">
-            <img src="/images/hab.png" className="p-8 mx-auto" style={{ height: '150px' }} />
+          <div style={cardBackground} className="rounded-lg p-6">
+            <img
+              src="/images/hab.png"
+              className="mx-auto p-8"
+              style={{ height: "150px" }}
+            />
             <h4 className="mt-4 text-lg font-bold text-electric-blue">
               Hackathons @ Berkeley
             </h4>
             <p className="mt-2">
-              is the non-profit organization that runs the largest collegiate hackathon in the world. Every year, Hackathons @ Berkeley brings together innovative minds to push the boundaries of what is possible.
+              is the non-profit organization that runs the largest collegiate
+              hackathon in the world. Every year, Hackathons @ Berkeley brings
+              together innovative minds to push the boundaries of what is
+              possible.
             </p>
           </div>
-          <div style={cardBackground} className="p-6 rounded-lg">
-            <img src="/images/berkeley-skydeck-logo.png" className="p-8 mx-auto" style={{ height: '150px', objectFit: 'contain', objectPosition: 'center' }} />
+          <div style={cardBackground} className="rounded-lg p-6">
+            <img
+              src="/images/berkeley-skydeck-logo.png"
+              className="mx-auto p-8"
+              style={{
+                height: "150px",
+                objectFit: "contain",
+                objectPosition: "center",
+              }}
+            />
             <h4 className="mt-4 text-lg font-bold text-electric-blue">
               Berkeley SkyDeck
             </h4>
             <p className="mt-2">
-              is the premier startup accelerator affiliated with UC Berkeley, with a mission to empower and cultivate the next generation of entrepreneurs.
+              is the premier startup accelerator affiliated with UC Berkeley,
+              with a mission to empower and cultivate the next generation of
+              entrepreneurs.
             </p>
           </div>
         </div>
-        <h2 className="text-5xl mt-24 font-pp-mondwest">
-          Key Advisors
-        </h2>
-        <div className="grid md:grid-cols-3 w-95 md:w-full max-w-5xl gap-6 mt-8 mb-64">
+        <h2 className="mt-24 font-pp-mondwest text-5xl">Key Advisors</h2>
+        <div className="w-95 mb-64 mt-8 grid max-w-5xl gap-6 md:w-full md:grid-cols-3">
           <div>
-            <img src="/images/oski.jpeg" className="rounded-full object-cover mx-auto" style={{ height: '175px', width: '175px' }} />
-            <h4 className="mt-4 text-lg font-bold text-center">
+            <img
+              src="/images/oski.jpeg"
+              className="mx-auto rounded-full object-cover"
+              style={{ height: "175px", width: "175px" }}
+            />
+            <h4 className="mt-4 text-center text-lg font-bold">
               Oski The Bear
             </h4>
             <p className="mt-2  text-center">
@@ -59,8 +92,12 @@ export default function Home() {
             </p>
           </div>
           <div>
-            <img src="/images/oski.jpeg" className="rounded-full object-cover mx-auto" style={{ height: '175px', width: '175px' }} />
-            <h4 className="mt-4 text-lg font-bold text-center">
+            <img
+              src="/images/oski.jpeg"
+              className="mx-auto rounded-full object-cover"
+              style={{ height: "175px", width: "175px" }}
+            />
+            <h4 className="mt-4 text-center text-lg font-bold">
               Oski The Bear
             </h4>
             <p className="mt-2  text-center">
@@ -68,8 +105,12 @@ export default function Home() {
             </p>
           </div>
           <div>
-            <img src="/images/oski.jpeg" className="rounded-full object-cover mx-auto" style={{ height: '175px', width: '175px' }} />
-            <h4 className="mt-4 text-lg font-bold text-center">
+            <img
+              src="/images/oski.jpeg"
+              className="mx-auto rounded-full object-cover"
+              style={{ height: "175px", width: "175px" }}
+            />
+            <h4 className="mt-4 text-center text-lg font-bold">
               Oski The Bear
             </h4>
             <p className="mt-2  text-center">
@@ -77,14 +118,13 @@ export default function Home() {
             </p>
           </div>
         </div>
-        
       </main>
-      <div className="z-0 margin-auto fixed bottom-[-50vh] left-1/2 h-[1144px] w-[1181px] w-full -translate-x-1/2 transform overflow-hidden rounded-full bg-gradient-radial blur-xl"></div>
+      <div className="margin-auto fixed bottom-[-50vh] left-1/2 z-0 h-[1144px] w-[1181px] w-full -translate-x-1/2 transform overflow-hidden rounded-full bg-gradient-radial blur-xl"></div>
       <style>
         {`html {
           background: #000
         }`}
-        </style>
-    </>
+      </style>
+    </main>
   );
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,37 +2,50 @@ import Hero from "@/components/Hero";
 import SlidingText from "@/components/SlidingText";
 import Nav from "@/components/Nav";
 
-const cardBackground = { backgroundImage: 'linear-gradient(90deg, rgba(76, 4, 110, 0.2508) 0.25%, rgba(27, 6, 152, 0.1914) 93.27%)' }
+const cardBackground = {
+  backgroundImage:
+    "linear-gradient(90deg, rgba(76, 4, 110, 0.2508) 0.25%, rgba(27, 6, 152, 0.1914) 93.27%)",
+};
 
 export default function Home() {
   return (
-    <>
+    <main className="bg-[url('../public/images/contact-bg.svg')] bg-cover">
       <Nav />
-      <main className="z-1 relative min-h-screen overflow-hidden flex flex-col items-center text-white" style={{ zIndex: 999 }}>
-        <img src="/images/four-point.png" className="mt-24" style={{ height: '100px' }} />
-        <h1 className="text-5xl mt-6 font-pp-mondwest">
-          Contact Us
-        </h1>
-        <p className="max-w-2xl text-xl text-center text-balance mt-4">
+      <main
+        className="z-1 relative flex min-h-screen flex-col items-center overflow-hidden text-white"
+        style={{ zIndex: 999 }}
+      >
+        <img
+          src="/images/four-point.png"
+          className="mt-24"
+          style={{ height: "100px" }}
+        />
+        <h1 className="mt-6 font-pp-mondwest text-5xl">Contact Us</h1>
+        <p className="mt-4 max-w-2xl text-balance text-center text-xl">
           Any questions? Let us know!
         </p>
-        <div className="w-95 max-w-3xl text-center mt-12 px-8 text-lg">
+        <div className="w-95 mt-12 max-w-3xl px-8 text-center text-lg">
           <div>
             <p className="mb-2">
-              We are here to help. Please check out FAQ, and email us if your question is not answered. We’ll get back to you as soon as we can!
-            </p><p className="mb-2"> 
-              Please be advised we cannot offer travel reimbursements at this time. We apologize for the inconvenience
-            </p><p className="text-bold text-electric-blue">
-                     <a href="mailto:team@calhacks.io">team@calhacks.io</a></p>
+              We are here to help. Please check out FAQ, and email us if your
+              question is not answered. We’ll get back to you as soon as we can!
+            </p>
+            <p className="mb-2">
+              Please be advised we cannot offer travel reimbursements at this
+              time. We apologize for the inconvenience
+            </p>
+            <p className="text-bold text-electric-blue">
+              <a href="mailto:team@calhacks.io">team@calhacks.io</a>
+            </p>
           </div>
         </div>
       </main>
-      <div className="z-0 margin-auto fixed bottom-[-50vh] left-1/2 h-[1144px] w-[1181px] w-full -translate-x-1/2 transform overflow-hidden rounded-full bg-gradient-radial blur-xl"></div>
+      <div className="margin-auto fixed bottom-[-50vh] left-1/2 z-0 h-[1144px] w-[1181px] w-full -translate-x-1/2 transform overflow-hidden rounded-full bg-gradient-radial blur-xl"></div>
       <style>
-      {`html {
+        {`html {
         background: #000
       }`}
       </style>
-    </>
+    </main>
   );
 }
