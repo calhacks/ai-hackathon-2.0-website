5b39462b1004a7d9aa177999d1b019cc6590b07a
aryanvichare <aryanvichare10@gmail.com>
2024-06-13 00:37:28 -0700

    Add more sponsors

--- components/SponsorsPartners.tsx
+++ components/SponsorsPartners.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-const sponsorList = [
+const partneringSchools = [
   {
     image: "/images/sponsors/hackdavis.png",
     link: "https://hackdavis.io/",
@@ -67,7 +67,7 @@ const sponsorList = [
 
 export default function SponsorsPartners() {
   return (
-    <div className="flex w-screen px-4 pb-16 pt-4 md:px-0 lg:pb-0">
+    <div className="mx-auto flex max-w-screen-lg px-8 pb-16 pt-4 sm:px-4 md:px-0 lg:pb-0">
       <div className="m-auto">
         <p className="mb-2 text-center font-pp-mondwest text-3xl text-white sm:text-4xl xl:text-5xl">
           Sponsors & Partners
@@ -106,25 +106,151 @@ export default function SponsorsPartners() {
             href="https://www.intel.com/"
             rel="noopener noreferrer"
             target="_blank"
-            className="relative z-50 m-0 -mt-1.5 h-[80px] w-[160px] p-0 sm:h-[100px] sm:w-[200px] xl:h-[125px] xl:w-[250px]"
+            className="relative z-50 m-0 h-[56px] w-[160px] p-0 sm:h-[70px] sm:w-[200px] xl:h-[87.5px] xl:w-[250px]"
           >
-            <Image src="/images/sponsors/intel.svg" alt="Intel Logo" fill />
+            <Image
+              src="/images/sponsors/intel.png"
+              alt="Intel Logo"
+              fill
+              objectFit="contain"
+            />
+          </a>
+          <a
+            href="https://www.openai.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="relative z-50 m-0 h-[56px] w-[160px] p-0 sm:h-[70px] sm:w-[200px] xl:h-[87.5px] xl:w-[250px]"
+          >
+            <Image
+              src="/images/sponsors/openai.svg"
+              alt="OpenAI Logo"
+              fill
+              objectFit="contain"
+            />
+          </a>
+        </div>
+        <div className="my-16 grid grid-cols-2 items-center justify-items-center gap-8 lg:grid-cols-3 lg:gap-10">
+          <a
+            href="https://www.you.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="relative z-50 m-0 -mt-1.5 h-[150px] w-full p-0"
+          >
+            <Image
+              src="/images/sponsors/you.png"
+              alt="You.com Logo"
+              fill
+              objectFit="contain"
+            />
+          </a>
+          <a
+            href="https://berkeleyfrontier.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="relative z-50 m-0 -mt-1.5 h-[150px] w-full p-0"
+          >
+            <Image
+              src="/images/sponsors/berkeley-frontier-fund.jpg"
+              alt="Berkeley Frontier Fund Logo"
+              fill
+              objectFit="contain"
+            />
           </a>
           <a
             href="https://www.aicventures.org/"
             rel="noopener noreferrer"
             target="_blank"
-            className="relative z-50 m-0 -mt-2 h-[85px] w-[160px] p-0 sm:h-[100px] sm:w-[200px] xl:h-[120px] xl:w-[250px]"
+            className="relative z-50 m-0 -mt-2 h-[150px] w-full p-0"
           >
             <Image
               src="/images/sponsors/aic.png"
               alt="AIC Ventures Logo"
               fill
+              objectFit="contain"
+            />
+          </a>
+          {/*  */}
+          <a
+            href="https://www.reachcapital.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="relative z-50 m-0 -mt-1.5 h-[150px] w-full"
+          >
+            <Image
+              src="/images/sponsors/reach-capital.svg"
+              alt="Reach Capital Logo"
+              fill
+              objectFit="contain"
+            />
+          </a>
+          <a
+            href="https://lsvp.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="relative z-50 m-0 -mt-1.5 h-[150px] w-full"
+          >
+            <Image
+              src="/images/sponsors/lightspeed.svg"
+              alt="Lightspeed Venture Partners Logo"
+              fill
+              objectFit="contain"
+            />
+          </a>
+          <a
+            href="https://www.mayfield.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="relative z-50 m-0 -mt-1.5 h-[150px] w-full"
+          >
+            <Image
+              src="/images/sponsors/mayfield.svg"
+              alt="Mayfield Logo"
+              fill
+              objectFit="contain"
+            />
+          </a>
+          <a
+            href="https://aws.amazon.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="relative z-50 m-0 -mt-1.5 h-[80px] w-full p-12"
+          >
+            <Image
+              src="/images/sponsors/aws.svg"
+              alt="AWS Logo"
+              fill
+              objectFit="contain"
+            />
+          </a>{" "}
+          <a
+            href="https://www.hume.ai/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="relative z-50 m-0 -mt-8 h-[150px] w-full"
+          >
+            <Image
+              src="/images/sponsors/hume.svg"
+              alt="Hume Logo"
+              fill
+              objectFit="contain"
+            />
+          </a>
+          <a
+            href="https://groq.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="relative z-50 col-span-full m-0 -mt-1.5 h-[70px] w-full min-[1030px]:col-span-1 min-[1030px]:h-[100px]"
+          >
+            <Image
+              src="/images/sponsors/groq.svg"
+              alt="Groq Logo"
+              fill
+              objectFit="contain"
             />
           </a>
         </div>
         <div className="grid grid-cols-2 justify-items-center gap-10 lg:grid-cols-4">
-          {sponsorList.map((logo, index) => {
+          {partneringSchools.map((logo, index) => {
             return (
               <a
                 key={index}
